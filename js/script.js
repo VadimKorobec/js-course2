4e9f6c16e5d938fdc62e0b815ddbf073d4f350f3
VadimKorobec <_s_e_t__@ukr.net>
2024-03-26 17:16:37 +0200

    add some code

--- js/script.js
+++ js/script.js
@@ -56,14 +56,129 @@
 //     console.log("woop woop");
 //   });
 
-const test = (time) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, time);
-  });
+// const test = (time) => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve();
+//     }, time);
+//   });
+// };
+
+// test(1000).then(() => {
+//   console.log("Woop Woop");
+// });
+
+// const promise = new Promise((resolve, reject) => {
+//   const canFulfill = Math.random() > 0.5;
+//   if (canFulfill) {
+//     resolve("Resolve");
+//   } else {
+//     reject("Reject");
+//   }
+// });
+
+// promise
+//   .then((result) => {
+//     return result;
+//   })
+//   .then((result) => {
+//     console.log(result + " " + "Woop Woop");
+//   })
+
+//   .catch((result) => {
+//     console.log(result);
+//   });
+
+// const names = ["Ivan", "Anna", "Ksenia", "Voldemart"];
+
+// const newName = names.filter((item) => item.length < 5);
+
+// console.log(newName);
+
+// const answers = ["IvAn", "AnnA", "Hello"];
+
+// const newAnswers = answers.map((item) => item.toLowerCase());
+
+// console.log(newAnswers);
+
+// const some = ["4", "gfgf", "gfgfg"];
+
+// console.log(some.some((item) => typeof item === "number"));
+
+// console.log(some.every((item) => typeof item === "string"));
+
+// const arr = ["apple", "pear", "plum"];
+
+// const total = arr.reduce((acc, item) => {
+//   return `${acc}, ${item}`;
+// });
+
+// // console.log(total);
+
+// const obj = {
+//   ivan: "persone",
+//   anna: "persone",
+//   dog: "animal",
+//   cat: "animal",
+// };
+
+// const newArray = Object.entries(obj);
+// const newArry1 = newArray
+//   .reduce((acc, item) => {
+//     return acc.concat(item);
+//   }, [])
+//   .filter((item) => item.length === 4);
+// console.log(newArry1);
+
+// const films = [
+//   {
+//     name: "Titanic",
+//     rating: 9,
+//   },
+//   {
+//     name: "Die hard 5",
+//     rating: 5,
+//   },
+//   {
+//     name: "Matrix",
+//     rating: 8,
+//   },
+//   {
+//     name: "Some bad film",
+//     rating: 4,
+//   },
+// ];
+
+// function showGoodFilms(arr) {
+//   const rating = arr.filter((item) => item.rating >= 8);
+//   console.log(rating);
+// }
+
+// console.log(showGoodFilms(films));
+
+// function showListOfFilms(arr) {
+//   const string = arr.map((item, i) => {
+//     item.id = i;
+//     return item;
+//   });
+//   console.log(string);
+// }
+
+// console.log(showListOfFilms(films));
+
+const funds = [
+  { amount: -1400 },
+  { amount: 2400 },
+  { amount: -1000 },
+  { amount: 500 },
+  { amount: 10400 },
+  { amount: -11400 },
+];
+
+const getPositiveIncomeAmount = (data) => {
+  const total = data.map((item) => item[1] >= 0);
+
+  console.log(total);
 };
 
-test(1000).then(() => {
-  console.log("Woop Woop");
-});
+console.log(getPositiveIncomeAmount(funds));
