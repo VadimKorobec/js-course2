563be9f16185291657e30fbfde53b1f8166cc063
VadimKorobec <_s_e_t__@ukr.net>
2024-03-28 17:39:34 +0200

    add some code

--- script.js
+++ script.js
@@ -34,3 +34,22 @@ localStorage.setItem("Alex", JSON.stringify(persone));
 const person = JSON.parse(localStorage.getItem("Alex"));
 
 console.log(person);
+
+// const ans = prompt("Your name");
+
+// const reg = /\d/;
+
+// console.log(ans.match(reg));
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+
+// const pass = prompt("Password");
+
+// console.log(pass.replace(reg, "*"));
+
+// console.log("12-34-56".replace(/-/g, ":"));
+
+const str = "My name is R2D2";
+
+console.log(str.replace(/\W/gi, ":"));
