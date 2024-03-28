f079c98d1aa69f39de3f938d747d7c1e62115c95
VadimKorobec <_s_e_t__@ukr.net>
2024-03-28 16:52:11 +0200

    add some code

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
-'use strict';
+const checkbox = document.querySelector("#checkbox");
+const form = document.querySelector("form");
+const change = document.querySelector("#color");
 
+if (localStorage.getItem("checked")) {
+  checkbox.checked = true;
+}
+
+checkbox.addEventListener("change", () => {
+  localStorage.setItem("checked", true);
+});
+
+if (localStorage.getItem("bg")) {
+  change.style.backgroundColor = "tomato";
+}
+
+change.addEventListener("click", () => {
+  if (localStorage.getItem("bg") === "changed") {
+    localStorage.removeItem("bg");
+    form.style.backgroundColor = "white";
+  } else {
+    localStorage.setItem("bg", "changed");
+    form.style.backgroundColor = "green";
+  }
+});
+
+const persone = {
+  name: "Alex",
+  age: 25,
+};
+
+localStorage.setItem("Alex", JSON.stringify(persone));
+
+const person = JSON.parse(localStorage.getItem("Alex"));
+
+console.log(person);
